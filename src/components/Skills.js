15f0195676752aae3dfcc4da4cc59c8db48fa65e
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-02-19 21:40:45 +0200

    Finhsed up the remaining React components 🐠

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,13 +5,36 @@ export default function Skills() {
   return (
     <section id="section-container">
         <div id="skills-title-container">
-            <h1 id="skills-title">Skills</h1>
+            <h1 id="skills-title">Software Skills</h1>
         </div>
 
         <hr className="hr" />
 
         <div id="skills-list-container">
-          
+			<div id="skills-list-container-left">
+				<ul>
+					<li>Javascript</li>
+					<li>ReactJS</li>
+					<li>MondoDB</li>
+					<li>Next.js</li>
+					<li>ExpressJS</li>
+				</ul>
+
+				
+			</div>
+			
+			<div id="skills-list-container-right">
+
+				<ul>
+					<li>Python</li>
+					<li>ReactJS</li>
+					<li>MondoDB</li>
+					<li>Next.js</li>
+					<li>ExpressJS</li>
+				</ul>
+
+			</div>
+
         </div>
     </section>
   )
--- src/components/WorkExp.js
+++ src/components/WorkExp.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import '../styles/workexp.css'
 
+
+const WorkExpComp = (props) => {
+  return (
+    <div id="work-exp-comp-container">
+
+		<div id="work-exp-comp-title">
+			<h3>{props.title}</h3>
+		</div>
+
+		<div id="work-exp-comp-text">
+			<h4>{props.text}</h4>
+		</div>
+
+    </div>
+  )
+}
 export default function WorkExp() {
   return (
     <div id="work-exp-container">
@@ -8,6 +24,13 @@ export default function WorkExp() {
         <div id="work-exp-title-container">
             <h1 id="work-exp-title">Work Experience</h1>
         </div>
+
+        <hr className="hr" />
+
+        <WorkExpComp title={"17 July 2017 – 21 July 2017"} text={"Reutech Radar Systems: Assisted in the financial department."}/>
+        <WorkExpComp title={"1 December 2017 – 16 January 2018"} text={"Reutech Radar Systems: Assisted in the financial department."}/>
+        <WorkExpComp title={"27 November 2018 – 8 January 2019"} text={"Reutech Radar Systems: Assisted in the financial department."}/>
+        <WorkExpComp title={"25 November 2019 - 5 December 2020"} text={"Reutech Radar Systems: Assisted in the financial department."}/>
 
 
     </div>
